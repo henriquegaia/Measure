4b6d85cace1496699a98b1f1f849d57cc6d94bf6
henrique moreira <henrique.gaia@gmail.com>
2018-10-14 14:19:43 +0200

    Add project files.

---
+++ Demos/Demos.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Measurements;
+
+namespace Demos
+{
+    class IntegerAllocVsClassAlloc
+    {
+        static readonly int arraySize = 5_000;
+        static readonly int n = 5_000;
+
+        static void Main(string[] args)
+        {
+            ExecutionTime.Measure("AllocateInteger", 5, () =>
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    AllocateInteger(i % n);
+                }
+            });
+
+            ExecutionTime.Measure("AllocateClass", 5, () =>
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    AllocateClass();
+                }
+            });
+        }
+
+        private static void AllocateInteger(int val)
+        {
+            int[] arr = new int[arraySize];
+            for (int i = 0; i < arraySize; i++)
+            {
+                arr[i] = val;
+            }
+        }
+
+        private static void AllocateClass()
+        {
+            IntegerAllocVsClassAlloc[] arr = new IntegerAllocVsClassAlloc[arraySize];
+            for (int i = 0; i < arraySize; i++)
+            {
+                arr[i] = new IntegerAllocVsClassAlloc();
+            }
+        }
+    }
+}
---
+++ Measurements/ExecutionTime.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+namespace Measurements
+{
+    public class ExecutionTime
+    {
+        public static void Measure(string what, int reps, Action action)
+        {
+            System.Runtime.GCSettings.LatencyMode = System.Runtime.GCLatencyMode.Batch;
+
+            action();
+
+            double[] results = new double[reps];
+
+            for (int i = 0; i < reps; i++)
+            {
+                Stopwatch sw = new Stopwatch();
+                sw.Start();
+                action();
+                sw.Stop();
+                results[i] = sw.Elapsed.TotalMilliseconds;
+                Console.WriteLine($"repetition {i}: {results[i]} Milliseconds");
+            }
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"{what} - average = {results.Average()}, min = {results.Min()}, max = {results.Max()}");
+            Console.ForegroundColor = ConsoleColor.White;
+
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            GC.Collect();
+        }
+    }
+}
