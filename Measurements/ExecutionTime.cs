82ae92146e5b63b66f07e5ce3abc592406b8938e
henriquegaia <henrique.gaia@gmail.com>
2018-11-14 06:06:03 +0100

    Created Result class

--- Measurements/ExecutionTime.cs
+++ Measurements/ExecutionTime.cs
@@ -8,13 +8,16 @@ namespace Measurements
 {
     public class ExecutionTime
     {
+        //public IEnumerable<string> Repetitions { get; set; }
+        //public IEnumerable<string> Results { get; set; }
+
         public static void Measure(string what, int reps, Action action)
         {
             System.Runtime.GCSettings.LatencyMode = System.Runtime.GCLatencyMode.Batch;
+            double[] results = new double[reps];
+            double min, max, avg;
 
             action();
-
-            double[] results = new double[reps];
 
             for (int i = 0; i < reps; i++)
             {
@@ -26,8 +29,14 @@ namespace Measurements
                 Console.WriteLine($"repetition {i}: {results[i]} Milliseconds");
             }
 
+            min = results.Min();
+            max = results.Max();
+            avg = results.Average();
+
+            Result result = new Result(what, avg, min, max);
+
             Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine($"{what} - average = {results.Average()}, min = {results.Min()}, max = {results.Max()}");
+            Console.WriteLine($"{what} - average = {avg}, min = {min}, max = {max}");
             Console.ForegroundColor = ConsoleColor.White;
 
             GC.Collect();
---
+++ Measurements/Result.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Measurements
+{
+    class Result
+    {
+        public DateTime CreatedAt { get; }
+        public string TestName { get; }
+        public double Average { get; }
+        public double Minimum { get; }
+        public double Maximum { get; }
+
+        public Result(string testName, double average, double minimum, double maximum)
+        {
+            CreatedAt = DateTime.Now;
+            TestName = testName;
+            Average = average;
+            Minimum = minimum;
+            Maximum = maximum;
+        }
+    }
+}
